e3a52c2251a0677c5cb7b444ed2b69aaac220416
ismail özacar <ismail.ozacar@hemenis.com>
2020-04-05 12:57:52 +0300

    Dependency Manager Injection

---
+++ src/Domain/Entity/Customer.php
@@ -0,0 +1,27 @@
+<?php
+namespace CleanPhp\Invoicer\Domain\Entity;
+
+class Customer extends AbstractEntity {
+  protected $name;
+  protected $email;
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function setName($name) {
+    $this->name = $name;
+    return $this;
+  }
+
+  public function getEmail() {
+    return $this->emailAddress;
+  }
+
+  public function setEmail($email) {
+    $this->email = $email;
+    return $this;
+  }
+}
+
+ ?>
---
+++ src/Domain/Entity/Invoice.php
@@ -0,0 +1,39 @@
+<?php
+namespace CleanPhp\Invoicer\Domain\Entitiy;
+
+class Invoice {
+  protected $order;
+  protected $invoiceDate;
+  protected $total;
+
+  public function getOrder() {
+    return $this->order;
+  }
+
+  public function setOrder($order) {
+    $this->order = $order;
+    return $this;
+  }
+
+  public function getInvoiceDate() {
+    return $this->invoiceDate;
+  }
+
+  public function setInvoiceDate($invoiceDate) {
+    $this->invoiceDate = $invoiceDate;
+    return $this;
+  }
+
+  public function getTotal() {
+    $this->total = $total;
+  }
+
+  public function setTotal($total) {
+    $this->total = $total;
+    return $this;
+  }
+}
+
+
+
+ ?>
